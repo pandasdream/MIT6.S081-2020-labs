bb5e8feff89e926821bed83c78f46a6935102a3b
pandasdream <1094914383@qq.com>
2024-07-13 09:20:24 +0000

    Another solution of barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -47,6 +47,25 @@ barrier()
   bstate.nthread ++;
   pthread_mutex_unlock(&bstate.barrier_mutex);
   pthread_cond_broadcast(&bstate.barrier_cond);
+
+  // Another Solution
+  // static void 
+  // barrier()
+  // {
+  //   pthread_mutex_lock(&bstate.barrier_mutex);
+
+  //   bstate.nthread++;
+
+  //   if(bstate.nthread == nthread){
+  //     bstate.round++;
+  //     bstate.nthread = 0;
+  //     pthread_cond_broadcast(&bstate.barrier_cond);
+  //   }else{
+  //     pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  //   }
+
+  //   pthread_mutex_unlock(&bstate.barrier_mutex);
+  // }
 }
 
 static void *
